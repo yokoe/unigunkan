963595eeab7240a1f8faaa9c17a744d39da5ac14
Sota <sota@pankaku.com>
2012-10-18 12:39:40 +0900

    Added folder ref support.

--- lib/unigunkan/processor.rb
+++ lib/unigunkan/processor.rb
@@ -58,16 +58,49 @@ class Unigunkan::Processor
     folders = OPTS[:folder_refs].split(",")
 
     STDOUT.puts "Adding folder refs..."
+
     file_refs_to_add = []
     build_files_to_add = []
+    resource_build_phase_files_to_add = []
+    entry_in_tree_to_add = []
+
     for folder in folders
       file_ref = SecureRandom.hex
       build_file = SecureRandom.hex
       file_refs_to_add << "#{file_ref} /* #{folder} */ = {isa = PBXFileReference; lastKnownFileType = folder; path = #{folder}; sourceTree = \"<group>\"; };"
       build_files_to_add << "#{build_file} /* #{folder} in Resources */ = {isa = PBXBuildFile; fileRef = #{file_ref} /* #{folder} */; };"
+      resource_build_phase_files_to_add << "#{build_file} /* #{folder} in Resources */,"
+      entry_in_tree_to_add << "#{file_ref} /* #{folder} */,"
     end
     puts build_files_to_add.join("\n")
     puts file_refs_to_add.join("\n")
+    puts resource_build_phase_files_to_add.join("\n")
+    puts entry_in_tree_to_add.join("\n")
+
+    add_block_after "/* Begin PBXBuildFile section */", build_files_to_add.join("\n")
+    add_block_after "/* Begin PBXFileReference section */", file_refs_to_add.join("\n")
+
+    # Rewrite ResourcesBuildPhase section
+    target = /Resources \*\/ = {\n\t\t\tisa = PBXResourcesBuildPhase;\n\t\t\tbuildActionMask = (.*?);\n\t\t\tfiles = \(/
+    mask = @src.scan(target)[0][0]
+    build_phases = "Resources */ = {\n\t\t\tisa = PBXResourcesBuildPhase;\n\t\t\tbuildActionMask = #{mask};\n\t\t\tfiles = (\n"
+    @src.gsub!(target, build_phases + resource_build_phase_files_to_add.join("\n"))
+
+    # Rewrite PBXGroup section
+    target = /\/\* Begin PBXGroup section \*\/\n\t\t(.*?)\n\t\t\tisa = PBXGroup;\n\t\t\tchildren = \(/
+      # isa = PBXGroup;
+      # children = \("
+    p @src.scan(target)
+    group_id = @src.scan(target)[0][0]
+    group = "/* Begin PBXGroup section */
+\t\t#{group_id}
+\t\t\tisa = PBXGroup;
+\t\t\tchildren = ("
+    @src.gsub!(target, group + "\n" + entry_in_tree_to_add.join("\n"))
+  end
+
+  def add_block_after(line, block)
+    @src.gsub!(line, line + "\n" + block)
   end
 
   def delete_original_project_file
